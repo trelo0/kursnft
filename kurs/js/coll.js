2a783b41b0b6ab3b30dde8601a5fe05e40953263
trelo0 <121427805+trelo0@users.noreply.github.com>
2023-05-10 12:00:39 +0300

    Add files via upload

---
+++ kurs/js/coll.js
@@ -0,0 +1,36 @@
+// Получаем данные из файла JSON
+fetch('./col/collection.json')
+    .then(response => response.json())
+    .then(data => {
+        // Получаем контейнер, в котором будут отображаться картинки
+        const container = document.getElementById('discover-items');
+
+        // Проходимся по каждой картинке и создаем элементы для их отображения
+        data.images.forEach(image => {
+            const item = document.createElement('div');
+            item.className = "item";
+
+
+            const img = document.createElement('img');
+            img.src = image.url;
+            img.className = "item-img";
+
+            const itemt = document.createElement('div');
+            itemt.className = "item-title";
+
+            const caption = document.createElement('p');
+            caption.textContent = image.caption;
+
+            const price = document.createElement('p');
+            price.textContent = image.price;
+
+
+            itemt.appendChild(caption)
+            itemt.appendChild(price)
+
+            item.appendChild(img)
+            item.appendChild(itemt)
+
+            container.appendChild(item);
+        });
+    });
